af181baba2e1a9058cc90d56ec31dd1825164fb2
Igor Karachentsev <kucheriavij@gmail.com>
2020-04-08 19:33:39 +0300

    initial commit

---
+++ vk-music-downloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vk_music_downloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
