50bc90331808db5710d7186656c06ce09c90e503
Igor Karachentsev <kucheriavij@gmail.com>
2020-04-08 22:44:49 +0300

    Create auth command

--- vk-music-downloader/Program.cs
+++ vk-music-downloader/Program.cs
@@ -1,12 +1,73 @@
-﻿using System;
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
 
-namespace vk_music_downloader
+namespace VkMusicDownloader
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CommandLineApplication app = new CommandLineApplication();
+
+            CommandLineApplication auth = app.Command("auth", config => {
+                config.Description = "Login or logout in https://vk.com";
+                config.HelpOption("-? | -h | --help");
+                config.OnExecute(() => {
+                    config.ShowHelp("auth");
+                    return 0;
+                });
+            });
+
+            auth.Command("login", config => {
+                config.Description = "Login in https://vk.com";
+                config.HelpOption("-? | -h | --help");
+
+                CommandArgument login = config.Argument("login", "Your email or phone number", false);
+                CommandArgument password = config.Argument("password", "Your password");
+
+                config.OnExecute(() => {
+                    string _login = login.Value;
+                    string _passworg = password.Value;
+
+                    if(string.IsNullOrWhiteSpace(_login) && string.IsNullOrWhiteSpace(_passworg))
+                    {
+                        Console.Out.WriteLine($"Arguments {login.Name} and {password.Name} not set. Please enter your email and passwor or run auth -h");
+                        
+                        return 0;
+                    }
+
+                    try
+                    {
+                        var auth = new VkAuth(_login, _passworg);
+                        string GetInfo = auth.Auth();
+
+                        switch (auth.CheckAuth(GetInfo))
+                        {
+                            case 1:
+                                Console.Out.WriteLine("You are logged");
+                                break;
+                            case 2:
+                                Console.Out.WriteLine("You have a two factor authorization");
+                                break;
+                            case 0:
+                                Console.Error.WriteLine("You login or password incorrect");
+                                break;
+                        }
+
+                        return 0;
+                    }
+                    catch (Exception E)
+                    {
+                        Console.Error.WriteLine($"Login faild. Check login or password. Err: {E}");
+
+                        return 0;
+                    }
+                });
+            });
+
+            app.HelpOption("-? | -h | --help");
+            var result = app.Execute(args);
+            Environment.Exit(result);
         }
     }
 }
---
+++ vk-music-downloader/VkAuth.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leaf.xNet;
+
+namespace VkMusicDownloader
+{
+    class VkAuth
+    {
+        const string VkLoginPage = "https://vk.com/login";
+        const string VkMainPage = "https://vk.com";
+        const string VkLoginUrl = "https://login.vk.com/?act=login";
+        private string Login { get; set; }
+        private string Password { get; set; }
+        private string Ip_H { get; set; }
+        private string Lg_H { get; set; }
+        private string Cookie { get; set; }
+
+        public VkAuth(string Login, string Password)
+        {
+            this.Login = Login;
+            this.Password = Password;
+            ParseDataAuth();
+        }
+
+        private HttpResponse GetAuth()
+        {
+            HttpRequest request = new HttpRequest();
+            request.AddHeader("Upgrade-Insecure-Requests", "1");
+            request.AddHeader("Host", "vk.com");
+            request.UserAgentRandomize();
+            request.KeepAlive = false;
+
+            HttpResponse response = request.Get(VkLoginPage);
+
+            return response;
+        }
+
+        private void ParseDataAuth()
+        {
+            var GetAuths = GetAuth();
+            Ip_H = System.Text.RegularExpressions.Regex.Match(GetAuths.ToString(), "name=\"ip_h\" value=\"(.*?)\"").Groups[1].Value;
+            Lg_H = System.Text.RegularExpressions.Regex.Match(GetAuths.ToString(), "name=\"lg_h\" value=\"(.*?)\"").Groups[1].Value;
+            Cookie = GetAuths.Cookies.GetCookieHeader(VkLoginPage);
+        }
+
+        public string Auth()
+        {
+            HttpRequest request = new HttpRequest();
+            request.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddHeader("Upgrade-Insecure-Requests", "1");
+            request.AddHeader("Cookie", Cookie);
+            request.AddHeader("Referer", VkLoginPage);
+            request.UserAgentRandomize();
+            request.KeepAlive = false;
+
+            RequestParams Params = new RequestParams();
+            Params["act"] = "login";
+            Params["role"] = "al_frame";
+            Params["_origin"] = VkMainPage;
+            Params["ip_h"] = Ip_H;
+            Params["lg_h"] = Lg_H;
+            Params["email"] = Login;
+            Params["pass"] = Password;
+
+            string response = request.Post(VkLoginUrl, Params).ToString();
+            Cookie = request.Cookies.GetCookieHeader(VkMainPage);
+
+            return response;
+        }
+
+        public int CheckAuth(string response)
+        {
+            int result = 0;
+
+            if (System.Text.RegularExpressions.Regex.Match(response, "Notifier.init((.*))").Groups[1].Value.Length > 0)
+            {
+                result = 1;
+            }
+            else if (response.Contains("try{parent.location.href='/login?act=authcheck'}catch(e){}"))
+            {
+                result = 2;
+            }
+            else if (response.Contains("parent.stManager.add(['notifier.js', 'notifier.css'], function() {"))
+            {
+                result = 0;
+            }
+
+            return result;
+        }
+    }
+}
